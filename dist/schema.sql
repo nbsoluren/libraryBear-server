a64c49de4d6c694cd7529ed2b672551f91132dd9
keithliam <keithliamm@gmail.com>
2018-07-10 08:49:51 +0800

    Update schema

--- dist/schema.sql
+++ dist/schema.sql
@@ -1,18 +1,7 @@
 DROP DATABASE IF EXISTS LibraryBear;
 CREATE DATABASE LibraryBear;
 
- USE LibraryBear;
-
-DROP TABLE IF EXISTS user;
-
-CREATE TABLE user (
-  name varchar(40) DEFAULT NULL,
-  id varchar(60) NOT NULL,
-  source varchar(256) NOT NULL,
-  prev_transac datetime NOT NULL,
-  PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-  
+USE LibraryBear;
 
 DROP TABLE IF EXISTS book;
 
@@ -27,3 +16,20 @@ CREATE TABLE book (
   CONSTRAINT user_book_fk FOREIGN KEY (borrower) REFERENCES user(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+DROP TABLE IF EXISTS user_group;
+
+CREATE TABLE user_group (
+  name varchar(256) NOT NULL,
+  user varchar(256) NOT NULL,
+  CONSTRAINT FK_user FOREIGN KEY (user) REFERENCES user (id) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS user;
+
+CREATE TABLE user (
+  name varchar(40) DEFAULT NULL,
+  id varchar(60) NOT NULL,
+  source varchar(256) NOT NULL,
+  prev_transac datetime NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
