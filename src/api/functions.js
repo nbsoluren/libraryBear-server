e560c4df651190ffc05c2ca905d9bd4637c68120
keithliam <keithliamm@gmail.com>
2018-07-10 12:34:19 +0800

    Update access token

--- src/api/functions.js
+++ src/api/functions.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 
 function pushToFacebook(id, payload) {
 	const url = 'https://graph.facebook.com/v2.6/me/messages?access_token=';
-	const pageAccessToken = 'token'
+	const pageAccessToken = 'EAAdaLXIFrz8BAI5jbBbAfBrV03Yl6TnFsvpqKsvfVdUZAkGO93ZCv0YRHfVrUAu1cd1vkK3J6fJNKnjYZByZAQsSC2y7hgXCfHuPxoDMqZCkNlpCpwGMAOA2UeDvkzyxxUhbPyyrSeTx9fiZCRBfP0bI0rRA1bTEWfAhqZB4uMPeM03ZAnSF5QkA'
 	
 	fetch(url + pageAccessToken, {
 		headers: { 'Content-Type': 'application/json' },
@@ -10,6 +10,7 @@ function pushToFacebook(id, payload) {
 		body: JSON.stringify(payload)	  	
 	})
 		.catch((e) => { console.log(e); });
+
 }
 
 export function pushMessage(id, message) {
