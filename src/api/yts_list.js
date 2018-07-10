478b1fb6f6392e981e742b4c199c8255021047ca
keithliam <keithliamm@gmail.com>
2018-07-10 15:00:02 +0800

    Update access token

--- src/api/yts_list.js
+++ src/api/yts_list.js
@@ -3,7 +3,7 @@ import * as cheerio from 'cheerio';
 
 function pushNotif(id, payload) {
     const url = 'https://graph.facebook.com/v2.6/me/messages?access_token=';
-    const pageAccessToken = 'EAALqslE8is4BAGSVjDTN5oCfT5aNlcgmQIbKOZALeLHUC9Ldc5aDZARu5jRMZBhwj5WnPzuFxL8BzTlsjKLZC90FLzYmXgbnhDo5i1Gr8X9ubIMdBSAD1wSIMwRBhSrxHYSrMLyz9rYi4mffoQAQO4eZBBCv9oL7zItyLjiIfIAZDZD'
+    const pageAccessToken = 'EAAdaLXIFrz8BAI5jbBbAfBrV03Yl6TnFsvpqKsvfVdUZAkGO93ZCv0YRHfVrUAu1cd1vkK3J6fJNKnjYZByZAQsSC2y7hgXCfHuPxoDMqZCkNlpCpwGMAOA2UeDvkzyxxUhbPyyrSeTx9fiZCRBfP0bI0rRA1bTEWfAhqZB4uMPeM03ZAnSF5QkA'
     
     fetch(url + pageAccessToken, {
         headers: { 'Content-Type': 'application/json' },
